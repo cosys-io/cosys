8e7945b03b58303e52863d0e28042a2709cf3d6d
Adwin Ang <adwin@aegir-technologies.com>
2024-06-16 18:15:39 +0800

    feat(ui): Update Radix UI components
    Added new Radix UI components:
    - @radix-ui/react-avatar@1.0.4
    - @radix-ui/react-checkbox@1.0.4
    - @radix-ui/react-tooltip@1.0.7
    - Updated dependencies for existing components.

--- ui/app/routes/login.tsx
+++ ui/app/routes/login.tsx
@@ -4,41 +4,37 @@ import { Input } from "@components/ui/input";
 import { Button } from "@components/ui/button";
 import { Form, Link } from "@remix-run/react";
 
-export default function Component() {
+export default function Login() {
     return (
-        <Card className="mx-auto max-w-sm">
-            <CardHeader>
-                <CardTitle className="text-2xl">Login</CardTitle>
-                <CardDescription>Enter your email below to login to your account</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Form method="POST">
-                    <div className="grid gap-4">
-                        <div className="grid gap-2">
-                            <Label htmlFor="email">Email</Label>
-                            <Input id="email" type="email" placeholder="m@example.com" required />
-                        </div>
-                        <div className="grid gap-2">
-                            <div className="flex items-center">
-                                <Label htmlFor="password">Password</Label>
-                                <Link to={"/reset-password"} className="ml-auto inline-block text-sm underline">
-                                    Forgot your password?
-                                </Link>
+        <div className="h-svh flex items-center justify-center">
+            <Card className="mx-auto max-w-sm my-auto">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardDescription>Enter your username below to login to your account</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form method="POST">
+                        <div className="grid gap-4">
+                            <div className="grid gap-2">
+                                <Label htmlFor="username">Username</Label>
+                                <Input id="username" type="text" name="username" required />
                             </div>
-                            <Input id="password" type="password" required />
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Password</Label>
+                                    <Link to="/reset-password" className="ml-auto inline-block text-sm underline">
+                                        Forgot your password?
+                                    </Link>
+                                </div>
+                                <Input id="password" type="password" required />
+                            </div>
+                            <Button type="submit" className="w-full">
+                                Login
+                            </Button>
                         </div>
-                        <Button type="submit" className="w-full">
-                            Login
-                        </Button>
-                    </div>
-                    <div className="mt-4 text-center text-sm">
-                        Don&apos;t have an account?{" "}
-                        <Link to={"/sign-up"} className="underline">
-                            Sign up
-                        </Link>
-                    </div>
-                </Form>
-            </CardContent>
-        </Card>
+                    </Form>
+                </CardContent>
+            </Card>
+        </div>
     );
 }
