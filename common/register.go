e543b437dd97e5dac0bfb29507bde317799951ef
coombunger <engyuhan@icloud.com>
2024-07-18 00:13:48 +0800

    feat(registers): Added permanent registers

--- common/register.go
+++ common/register.go
@@ -14,21 +14,17 @@ import (
 
 // options are configurations for registers.
 type options struct {
-	itemName    string
-	checkZero   bool
-	allowUpdate bool
-	allowRemove bool
-	checkExist  bool
+	itemName   string
+	checkZero  bool
+	checkExist bool
 }
 
 // defaultOptions returns the default configuration.
 func defaultOptions() options {
 	return options{
-		itemName:    "item",
-		checkZero:   true,
-		allowUpdate: false,
-		allowRemove: false,
-		checkExist:  true,
+		itemName:   "item",
+		checkZero:  true,
+		checkExist: true,
 	}
 }
 
@@ -49,16 +45,6 @@ var skipCheckZero option = func(opts *options) {
 	opts.checkZero = false
 }
 
-// allowUpdate allows the register item to be updated.
-var allowUpdate option = func(opts *options) {
-	opts.allowUpdate = true
-}
-
-// allowRemove allows the register item to be removed.
-var allowRemove option = func(opts *options) {
-	opts.allowRemove = true
-}
-
 // skipCheckExist will cause an error to be thrown when updating or removing an unregistered value.
 var skipCheckExist option = func(opts *options) {
 	opts.checkExist = true
@@ -68,7 +54,7 @@ var skipCheckExist option = func(opts *options) {
 
 // singleRegister is a register for a single value.
 type singleRegister[T any] struct {
-	mutex      sync.RWMutex
+	mutex      *sync.RWMutex
 	register   T
 	registered bool
 	options    options
@@ -82,17 +68,17 @@ func newSingleRegister[T any](cfg ...option) *singleRegister[T] {
 	}
 
 	return &singleRegister[T]{
-		mutex:      sync.RWMutex{},
+		mutex:      &sync.RWMutex{},
 		register:   *new(T),
 		registered: false,
 		options:    opts,
 	}
 }
 
-// Get returns the registered value and throws an error
+// Get returns the registered value and returns an error
 // if a value has not been registered.
 // Safe for concurrent use.
-func (r *singleRegister[T]) Get() (T, error) {
+func (r singleRegister[T]) Get() (T, error) {
 	r.mutex.RLock()
 	defer r.mutex.RUnlock()
 
@@ -105,12 +91,12 @@ func (r *singleRegister[T]) Get() (T, error) {
 
 // Clone returns a cloned single register
 // Safe for concurrent use.
-func (r *singleRegister[T]) Clone() *singleRegister[T] {
+func (r singleRegister[T]) Clone() *singleRegister[T] {
 	r.mutex.RLock()
 	defer r.mutex.RUnlock()
 
 	return &singleRegister[T]{
-		mutex:      sync.RWMutex{},
+		mutex:      &sync.RWMutex{},
 		register:   r.register,
 		registered: r.registered,
 
@@ -118,11 +104,11 @@ func (r *singleRegister[T]) Clone() *singleRegister[T] {
 	}
 }
 
-// Register sets the registered value, and throws an error
+// Register sets the registered value, and returns an error
 // if the checkZero configuration is true and the value is a zero-value,
 // or if a value has already been registered.
 // Safe for concurrent use.
-func (r *singleRegister[T]) Register(item T) error {
+func (r singleRegister[T]) Register(item T) error {
 	if r.options.checkZero && isZero(item) {
 		return fmt.Errorf("%s is nil", r.options.itemName)
 	}
@@ -140,80 +126,32 @@ func (r *singleRegister[T]) Register(item T) error {
 	return nil
 }
 
-// Update sets a new registered value, and throws an error
-// if allowUpdate configuration is false,
-// or if the checkZero configuration is true and the value is a zero-value,
-// or if the mustExist configuration is true and a value has not been registered or has been removed.
-// Safe for concurrent use.
-func (r *singleRegister[T]) Update(item T) error {
-	if !r.options.allowUpdate {
-		return fmt.Errorf("%s cannot be updated", r.options.itemName)
-	}
+// Permanent Register
 
-	if r.options.checkZero && isZero(item) {
-		return fmt.Errorf("%s is nil", r.options.itemName)
-	}
-
-	r.mutex.Lock()
-	defer r.mutex.Unlock()
-
-	if r.options.checkExist && !r.registered {
-		return fmt.Errorf("%s is not registered", r.options.itemName)
-	}
-
-	r.register = item
-
-	return nil
-}
-
-// Remove deletes the registered value, and throws an error
-// if the allowRemove configuration is false,
-// or if the mustExists configuration is true and a value has not been registered or has been removed.
-// Safe for concurrent use.
-func (r *singleRegister[T]) Remove() error {
-	if !r.options.allowRemove {
-		return fmt.Errorf("%s cannot be removed", r.options.itemName)
-	}
-
-	r.mutex.Lock()
-	defer r.mutex.Unlock()
-
-	if r.options.checkExist && !r.registered {
-		return fmt.Errorf("%s is not registered", r.options.itemName)
-	}
-
-	r.register = *new(T)
-	r.registered = false
-
-	return nil
-}
-
-// Multi Register
-
-// multiRegister is a register for multiple values.
-type multiRegister[T any] struct {
-	mutex    sync.RWMutex
+// permRegister is a register for multiple values which cannot be updated or deleted.
+type permRegister[T any] struct {
+	mutex    *sync.RWMutex
 	register map[string]T
 	options  options
 }
 
-// newMultiRegister returns a new multi register with configurations.
-func newMultiRegister[T any](cfg ...option) *multiRegister[T] {
+// newPermRegister returns a new permanent register with configurations.
+func newPermRegister[T any](cfg ...option) *permRegister[T] {
 	opts := defaultOptions()
 	for _, opt := range cfg {
 		opt(&opts)
 	}
 
-	return &multiRegister[T]{
-		mutex:    sync.RWMutex{},
+	return &permRegister[T]{
+		mutex:    &sync.RWMutex{},
 		register: make(map[string]T),
 		options:  opts,
 	}
 }
 
-// Get return the registered value with the given uid, and throws an error
+// Get return the registered value with the given uid, and returns an error
 // if a value has not been registered under that uid.
-func (r *multiRegister[T]) Get(uid string) (T, error) {
+func (r permRegister[T]) Get(uid string) (T, error) {
 	r.mutex.RLock()
 	defer r.mutex.RUnlock()
 
@@ -227,7 +165,7 @@ func (r *multiRegister[T]) Get(uid string) (T, error) {
 
 // GetAll returns a map of all registered values.
 // Safe for concurrent use, returns a copy of underlying map.
-func (r *multiRegister[T]) GetAll() map[string]T {
+func (r permRegister[T]) GetAll() map[string]T {
 	r.mutex.RLock()
 	defer r.mutex.RUnlock()
 
@@ -236,23 +174,23 @@ func (r *multiRegister[T]) GetAll() map[string]T {
 
 // Clone returns a cloned multi register.
 // Safe for concurrent use.
-func (r *multiRegister[T]) Clone() *multiRegister[T] {
+func (r permRegister[T]) Clone() *permRegister[T] {
 	r.mutex.RLock()
 	defer r.mutex.RUnlock()
 
-	return &multiRegister[T]{
-		mutex:    sync.RWMutex{},
+	return &permRegister[T]{
+		mutex:    &sync.RWMutex{},
 		register: maps.Clone(r.register),
 
 		options: r.options,
 	}
 }
 
-// Register sets a value under the given uid, and throws an error
+// Register sets a value under the given uid, and returns an error
 // if the checkZero configuration is true and the value is a zero-value,
 // of if a value has been registered under that uid.
 // Safe for concurrent use.
-func (r *multiRegister[T]) Register(uid string, item T) error {
+func (r permRegister[T]) Register(uid string, item T) error {
 	if r.options.checkZero && isZero(item) {
 		return fmt.Errorf("%s is nil: %s", r.options.itemName, uid)
 	}
@@ -269,12 +207,12 @@ func (r *multiRegister[T]) Register(uid string, item T) error {
 	return nil
 }
 
-// RegisterMany sets a map of values under their corresponding uid, and throws an error
+// RegisterMany sets a map of values under their corresponding uid, and returns an error
 // if the checkZero configuration is true and any value is a zero-value
 // or if a value has been registered under any uid.
 // The operation is atomic, either all or no values will be set.
 // Safe for concurrent use.
-func (r *multiRegister[T]) RegisterMany(items map[string]T) error {
+func (r permRegister[T]) RegisterMany(items map[string]T) error {
 	if r.options.checkZero {
 		if err := anyZero(r.options.itemName, items); err != nil {
 			return err
@@ -295,13 +233,13 @@ func (r *multiRegister[T]) RegisterMany(items map[string]T) error {
 	return nil
 }
 
-// RegisterRandom sets a value under a random uid, and returns the uid or throws an error
+// RegisterRandom sets a value under a random uid, and returns the uid or returns an error
 // if the checkZero configuration is true and the value is a zero-value,
 // or if it fails to generate a valid random uid.
 // The generated uid are all prefixed with '$', if using both Register and RegisterRandom,
 // do not use uid prefixed by $ for Register.
 // Safe for concurrent use.
-func (r *multiRegister[T]) RegisterRandom(item T) (string, error) {
+func (r permRegister[T]) RegisterRandom(item T) (string, error) {
 	if r.options.checkZero && isZero(item) {
 		return "", fmt.Errorf("%s is nil", r.options.itemName)
 	}
@@ -326,16 +264,37 @@ func (r *multiRegister[T]) RegisterRandom(item T) (string, error) {
 	return uid, nil
 }
 
-// Update sets a new value under the given uid, and throws an error
+// Multi Register
+
+// multiRegister is a register for multiple values.
+type multiRegister[T any] struct {
+	*permRegister[T]
+}
+
+// newMultiRegister returns a new multi register with configurations.
+func newMultiRegister[T any](cfg ...option) *multiRegister[T] {
+	return &multiRegister[T]{
+		newPermRegister[T](cfg...),
+	}
+}
+
+// Clone returns a cloned multi register.
+// Safe for concurrent use.
+func (r multiRegister[T]) Clone() *multiRegister[T] {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
+
+	return &multiRegister[T]{
+		r.permRegister.Clone(),
+	}
+}
+
+// Update sets a new value under the given uid, and returns an error
 // if the allowUpdate configuration is false,
 // or if the checkZero configuration is true and the value is a zero-value,
 // or if the mustExist configuration is true and no value has been registered under that uid.
 // Safe for concurrent use.
-func (r *multiRegister[T]) Update(uid string, item T) error {
-	if !r.options.allowUpdate {
-		return fmt.Errorf("%s cannot be updated", r.options.itemName)
-	}
-
+func (r multiRegister[T]) Update(uid string, item T) error {
 	if r.options.checkZero && isZero(item) {
 		return fmt.Errorf("%s is nil: %s", r.options.itemName, uid)
 	}
@@ -352,15 +311,11 @@ func (r *multiRegister[T]) Update(uid string, item T) error {
 	return nil
 }
 
-// Remove deletes the value under the given uid, and throws an error
+// Remove deletes the value under the given uid, and returns an error
 // if the allowRemove configuration is false,
 // or if the mustExist configuration is true and no value has been registered under that uid.
 // Safe for concurrent use.
-func (r *multiRegister[T]) Remove(uid string) error {
-	if !r.options.allowRemove {
-		return fmt.Errorf("%s cannot be removed", r.options.itemName)
-	}
-
+func (r multiRegister[T]) Remove(uid string) error {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
 
@@ -376,29 +331,24 @@ func (r *multiRegister[T]) Remove(uid string) error {
 // Stringer Register
 
 // stringerRegister is a register of stringer values.
-type stringerRegister[T fmt.Stringer] multiRegister[T]
+type stringerRegister[T fmt.Stringer] struct {
+	*multiRegister[T]
+}
 
 // newStringerRegister returns a new stringer register with configurations.
 func newStringerRegister[T fmt.Stringer](cfg ...option) *stringerRegister[T] {
-	opts := defaultOptions()
-	for _, opt := range cfg {
-		opt(&opts)
-	}
-
 	return &stringerRegister[T]{
-		mutex:    sync.RWMutex{},
-		register: make(map[string]T),
-		options:  opts,
+		newMultiRegister[T](cfg...),
 	}
 }
 
 // RegisterStringers sets a slice of stringer values under their respective String() return values,
-// and throws an error if any values have the same String() return values,
+// and returns an error if any values have the same String() return values,
 // or if the checkZero configuration is true and any value is a zero-value,
 // or if a value has been registered under any String() return value.
 // The operation is atomic, either all or no values will be set.
 // Safe for concurrent use.
-func (r *stringerRegister[T]) RegisterStringers(items ...T) error {
+func (r stringerRegister[T]) RegisterStringers(items ...T) error {
 	itemMap, err := toMap(r.options.itemName, items)
 	if err != nil {
 		return err
@@ -422,6 +372,12 @@ func (r *stringerRegister[T]) RegisterStringers(items ...T) error {
 	}
 
 	return nil
+}
+
+func (r stringerRegister[T]) Clone() *stringerRegister[T] {
+	return &stringerRegister[T]{
+		r.multiRegister.Clone(),
+	}
 }
 
 // Utils
@@ -507,7 +463,7 @@ func isStringer[T any]() bool {
 	return ok
 }
 
-// toMap converts a generic slice into a map, and throws an error
+// toMap converts a generic slice into a map, and returns an error
 // if any entry is not a Stringer.
 func toMap[T any](itemName string, itemSlice []T) (map[string]T, error) {
 	itemMap := map[string]T{}
