2f6bc8f2cda434a672f218e40d341bd8e52dcbcf
Adwin Ang <adwin@aegir.co>
2024-07-17 14:40:11 +0800

    Ammendments based on comments

--- common/service.go
+++ common/service.go
@@ -10,9 +10,8 @@ import (
 type Service map[string]any
 
 // A singleton Service map to be used throughout the application
-var singleService *Service
+var singleService Service = make(Service)
 
-var getLock = &sync.Mutex{}
 var registerLock = &sync.Mutex{}
 
 func isFunc(v any) bool {
@@ -27,55 +26,44 @@ func keyCollision(key string, service Service) bool {
 // RegisterServices registers a map of services to the singleton Service map
 // RegisterServices will return an error if a service name already exists in the singleton Service map
 // Safe for concurrent use
-func RegisterServices(services Service) (bool, error) {
+func RegisterServices(services Service) error {
 	serviceSingleton := GetService()
 	registerLock.Lock()
 	defer registerLock.Unlock()
 	for key, value := range services {
 		if !isFunc(value) {
-			return false, fmt.Errorf("service %s is not a function", key)
+			return fmt.Errorf("service %s is not a function", key)
 		}
-		if keyCollision(key, *serviceSingleton) {
-			return false, errors.New("service name already exists")
+		if keyCollision(key, serviceSingleton) {
+			return errors.New("service name already exists")
 		}
-		(*serviceSingleton)[key] = value
+		serviceSingleton[key] = value
 	}
 
-	return true, nil
+	return nil
 }
 
 // RegisterService registers a service to the singleton Service map
 // RegisterService will return an error if the service name already exists in the singleton Service map
 // Safe for concurrent use
-func RegisterService(serviceName string, service any) (bool, error) {
+func RegisterService(serviceName string, service any) error {
 	if !isFunc(service) {
-		return false, fmt.Errorf("service %s is not a function", serviceName)
+		return fmt.Errorf("service %s is not a function", serviceName)
 	}
 
 	serviceSingleton := GetService()
 	registerLock.Lock()
 	defer registerLock.Unlock()
-	if keyCollision(serviceName, *serviceSingleton) {
-		return false, errors.New("service name already exists")
+	if keyCollision(serviceName, serviceSingleton) {
+		return errors.New("service name already exists")
 	}
-	(*serviceSingleton)[serviceName] = service
+	serviceSingleton[serviceName] = service
 
-	return true, nil
+	return nil
 }
 
 // GetService returns the singleton Service map
-// If the singleton Service map does not exist, it will create one
-// Safe for concurrent use
-func GetService() *Service {
-	if singleService == nil {
-		getLock.Lock()
-		defer getLock.Unlock()
-		if singleService == nil {
-			fmt.Println("Creating service instance now.")
-			*singleService = make(Service)
-		} else {
-			fmt.Println("Service instance already created.")
-		}
-	}
+// Unsafe for concurrent use as it exposes the underlying singleService object to everyone
+func GetService() Service {
 	return singleService
 }
