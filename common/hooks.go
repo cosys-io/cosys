f5395118dc3ceae14180aed736dcd4a612cc03b5
coombunger <engyuhan@icloud.com>
2024-07-20 21:16:54 +0800

    feat(hooks): Updated hooks to not implement stringers.

--- common/hooks.go
+++ common/hooks.go
@@ -1,27 +1,17 @@
 package common
 
-type BootstrapHook struct {
-	uid  string
-	hook func(*Cosys) error
-}
+// BootstrapHook is a hook called during the bootstrap stage of the cosys app.
+type BootstrapHook func(*Cosys) error
 
+// Call calls the bootstrap hook.
 func (b BootstrapHook) Call(cosys *Cosys) error {
-	return b.hook(cosys)
+	return b(cosys)
 }
 
-func (b BootstrapHook) String() string {
-	return b.uid
-}
+// CleanupHook is a hook called during the cleanup stage of the cosys app.
+type CleanupHook func(*Cosys) error
 
-type CleanupHook struct {
-	uid  string
-	hook func(*Cosys) error
-}
-
+// Call calls the cleanup hook.
 func (c CleanupHook) Call(cosys *Cosys) error {
-	return c.hook(cosys)
-}
-
-func (c CleanupHook) String() string {
-	return c.uid
+	return c(cosys)
 }
