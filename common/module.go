e72c93a7ecf781a7eace92946a2a5100b22db398
coombunger <engyuhan@icloud.com>
2024-07-20 21:17:20 +0800

    feat(modules): Updated module register to use a permanent register.

--- common/module.go
+++ common/module.go
@@ -1,30 +1,18 @@
 package common
 
 import (
-	"fmt"
 	"sync"
 )
 
 var (
 	mdMutex    sync.RWMutex
-	mdRegister = make(map[string]Module)
+	mdRegister = newPermRegister[Module]()
 )
 
+// RegisterModule registers a module to the cosys app.
 func RegisterModule(moduleName string, module Module) error {
-	if module == nil {
-		return fmt.Errorf("module is nil: %s", moduleName)
-	}
-
-	mdMutex.Lock()
-	defer mdMutex.Unlock()
-
-	if _, dup := mdRegister[moduleName]; dup {
-		return fmt.Errorf("duplicate module:" + moduleName)
-	}
-
-	mdRegister[moduleName] = module
-
-	return nil
+	return mdRegister.Register(moduleName, module)
 }
 
+// Module is a hook that is called during the registration stage.
 type Module func(*Cosys) error
