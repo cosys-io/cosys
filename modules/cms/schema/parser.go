c11259e88815d1315db680be49497499762ec597
coombunger <engyuhan@icloud.com>
2024-08-14 21:17:38 +0800

    feat(cms): Added cms schema parser

---
+++ modules/cms/schema/parser.go
@@ -0,0 +1,131 @@
+package schema
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/cosys-io/cosys/common"
+	"io"
+)
+
+func ParseSchema(schema *ModelSchema, reader io.Reader) error {
+	schemaZero := modelSchemaZero{}
+
+	if err := json.NewDecoder(reader).Decode(&schemaZero); err != nil {
+		return err
+	}
+
+	parsedSchema, err := schemaZero.Schema()
+	if err != nil {
+		return err
+	}
+
+	*schema = *parsedSchema
+	return nil
+}
+
+type modelSchemaZero struct {
+	ModelType      string                 `yaml:"modelType" json:"modelType"`
+	CollectionName string                 `yaml:"collectionName" json:"collectionName"`
+	DisplayName    string                 `yaml:"displayName" json:"displayName"`
+	SingularName   string                 `yaml:"singularName" json:"singularName"`
+	PluralName     string                 `yaml:"pluralName" json:"pluralName"`
+	Description    string                 `yaml:"description" json:"description"`
+	Attributes     []*attributeSchemaZero `yaml:"attributes" json:"attributes"`
+}
+
+func (m modelSchemaZero) Schema() (*ModelSchema, error) {
+	if m.DisplayName == "" {
+		return nil, fmt.Errorf("model has no display name")
+	}
+	if m.ModelType == "" {
+		return nil, fmt.Errorf("model has no model type: %s", m.DisplayName)
+	}
+	if m.CollectionName == "" {
+		return nil, fmt.Errorf("model has no collection name: %s", m.DisplayName)
+	}
+	if m.SingularName == "" {
+		return nil, fmt.Errorf("model has no singular name: %s", m.DisplayName)
+	}
+	if m.PluralName == "" {
+		return nil, fmt.Errorf("model has no plural name: %s", m.DisplayName)
+	}
+
+	attrs := make([]common.AttributeSchema, len(m.Attributes))
+
+	for index, attr := range m.Attributes {
+		attrSchema, err := attr.Schema()
+		if err != nil {
+			return nil, err
+		}
+		attrs[index] = attrSchema
+	}
+
+	return &ModelSchema{
+		modelType:      m.ModelType,
+		collectionName: m.CollectionName,
+		displayName:    m.DisplayName,
+		singularName:   m.SingularName,
+		pluralName:     m.PluralName,
+		description:    m.Description,
+		attributes:     attrs,
+	}, nil
+}
+
+type attributeSchemaZero struct {
+	Name               string `yaml:"name" json:"name"`
+	SimplifiedDataType string `yaml:"simplifiedDataType" json:"simplifiedDataType"`
+	DetailedDataType   string `yaml:"detailedDataType" json:"detailedDataType"`
+
+	ShownInTable *bool     `yaml:"shownInTable" json:"shownInTable"`
+	Required     *bool     `yaml:"required" json:"required"`
+	Max          *int64    `yaml:"max" json:"max"`
+	Min          *int64    `yaml:"min" json:"min"`
+	MaxLength    *int      `yaml:"maxLength" json:"maxLength"`
+	MinLength    *int      `yaml:"minLength" json:"minLength"`
+	Private      *bool     `yaml:"private" json:"private"`
+	Editable     *bool     `yaml:"editable" json:"editable"`
+	Enum         *[]string `yaml:"enum" json:"enum"`
+
+	Default  *string `yaml:"default" json:"default"`
+	Nullable *bool   `yaml:"nullable" json:"nullable"`
+	Unique   *bool   `yaml:"unique" json:"unique"`
+}
+
+func (a attributeSchemaZero) Schema() (*AttributeSchema, error) {
+	if a.Name == "" {
+		return nil, fmt.Errorf("attribute has no name")
+	}
+	if a.SimplifiedDataType == "" {
+		return nil, fmt.Errorf("attribute has no simple type: %s", a.Name)
+	}
+	if a.DetailedDataType == "" {
+		return nil, fmt.Errorf("attribute has no detailed type: %s", a.Name)
+	}
+
+	return &AttributeSchema{
+		name:               a.Name,
+		simplifiedDataType: a.SimplifiedDataType,
+		detailedDataType:   a.DetailedDataType,
+
+		shownInTable: checkDefault(true, a.ShownInTable),
+		required:     checkDefault(false, a.Required),
+		max:          checkDefault(int64(2147483647), a.Max),
+		min:          checkDefault(int64(-2147483648), a.Min),
+		maxLength:    checkDefault(-1, a.MaxLength),
+		minLength:    checkDefault(-1, a.MinLength),
+		private:      checkDefault(false, a.Private),
+		editable:     checkDefault(true, a.Editable),
+		enum:         checkDefault([]string{}, a.Enum),
+
+		defaultValue: checkDefault("", a.Default),
+		nullable:     checkDefault(true, a.Nullable),
+		unique:       checkDefault(false, a.Unique),
+	}, nil
+}
+
+func checkDefault[T any](defaultValue T, value *T) T {
+	if value == nil {
+		return defaultValue
+	}
+	return *value
+}
