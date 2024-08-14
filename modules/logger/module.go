58469cbd74ba62209b5e24c1cf56e5de9a979f89
coombunger <engyuhan@icloud.com>
2024-08-14 23:15:53 +0800

    doc(logger): Added documentation for the logger module

--- modules/logger/module.go
+++ modules/logger/module.go
@@ -5,6 +5,7 @@ import (
 	"github.com/cosys-io/cosys/modules/logger/internal"
 )
 
+// init registers the module to register the Logger core service.
 func init() {
 	_ = common.RegisterModule(func(cosys *common.Cosys) error {
 		return cosys.UseLogger(internal.Logger{})
